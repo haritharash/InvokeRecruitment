3b0ebee4d061fde93f2124852e424485478e6602
haritharash <haritharash@gmail.com>
2020-06-11 15:44:30 +0800

    Updates

---
+++ precache-manifest.3600c4190cdc2746c2913db4c004f0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224d59fb499e25d6d0051760fdac377b",
+    "url": "/InvokeRecruitment/index.html"
+  },
+  {
+    "revision": "dc661512dcf43a8b64ea",
+    "url": "/InvokeRecruitment/static/css/main.6bcaed90.chunk.css"
+  },
+  {
+    "revision": "0262d744e4aca6a69fee",
+    "url": "/InvokeRecruitment/static/js/2.ac97489e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/InvokeRecruitment/static/js/2.ac97489e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc661512dcf43a8b64ea",
+    "url": "/InvokeRecruitment/static/js/main.012010e6.chunk.js"
+  },
+  {
+    "revision": "01a832a40f9d1f024c81",
+    "url": "/InvokeRecruitment/static/js/runtime-main.466307cb.js"
+  }
+]);
